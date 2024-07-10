4b67d1d7996c0f8ee388f15ff9edd28023a8603e
HlibPavlyk <pavlikgo78@gmail.com>
2024-07-10 23:14:04 +0300

    feat: add type config to entities

---
+++ src/ChatApp.Infrastructure/EntityTypeConfiguration/ChatConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatApp.Infrastructure.EntityTypeConfiguration;
+
+internal class ChatConfiguration : IEntityTypeConfiguration<Chat>
+{
+    public void Configure(EntityTypeBuilder<Chat> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .IsRequired();
+
+        builder.Property(x => x.Author)
+            .IsRequired();
+
+        builder.HasMany(x => x.Messages)
+            .WithOne(x => x.Chat)
+            .HasForeignKey(x => x.ChatId);
+    }
+}
---
+++ src/ChatApp.Infrastructure/EntityTypeConfiguration/MessageConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatApp.Infrastructure.EntityTypeConfiguration;
+
+internal class MessageConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Text)
+            .IsRequired();
+        
+        builder.Property(x => x.Timestamp)
+            .IsRequired();
+        
+        builder.HasOne(x => x.Chat)
+            .WithMany(x => x.Messages)
+            .HasForeignKey(x => x.ChatId);
+    }
+}
