7a2d2641e4be473d7e5b16adb728c83b7b466e5e
HlibPavlyk <pavlikgo78@gmail.com>
2024-07-10 22:41:24 +0300

    feat: add chat entities
    - add chat entity
    - add message entity

---
+++ src/ChatApp.Domain/Entities/Chat.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Domain.Entities;
+
+public class Chat
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    
+    public ICollection<Message> Messages { get; set; }
+}
