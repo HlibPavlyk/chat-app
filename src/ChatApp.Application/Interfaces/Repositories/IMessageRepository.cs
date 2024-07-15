c13ef9781ac2d054a6773a1aad4c976c99b4a4fb
HlibPavlyk <pavlikgo78@gmail.com>
2024-07-15 19:02:44 +0300

    chore: make minor changes

--- src/ChatApp.Application/Interfaces/Repositories/IMessageRepository.cs
+++ src/ChatApp.Application/Interfaces/Repositories/IMessageRepository.cs
@@ -3,7 +3,7 @@ using ChatApp.Domain.Entities;
 
 namespace ChatApp.Application.Interfaces.Repositories;
 
-public interface IMessageRepository
+public interface IMessageRepository : IGenericRepository<Message>
 {
     Task<Message?> GetMessageWithChatByIdAsync(int id);
     Task<PagedResponse<Message>?> GetAllPagedMessagesWithChatAsync(int page, int size);
